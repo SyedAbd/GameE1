fc12c9c1e54992b250d789d6a70924b706d56273
Yue Ding <ae8273@student.jamk.fi>
2024-04-14 14:51:13 +0300

    character

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController cc;
+    private Cameralook cameralook;
+    [SerializeField] private float crouchSpeed = 2f;
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float runSpeed = 7f;
+    [SerializeField] private float jumpForce = 5.5f;
+    [SerializeField] private float crouchTransitionSpeed = 5f;
+    [SerializeField] private float gravity = -7f;
+
+    private float gravityAcceleration;
+    private float yVelocity;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        cam = GetComponent<CameraLook>();
+
+        gravityAcceleration = gravity * gravity;
+        gravityAcceleration *= Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector3 moveDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            moveDir.z += 1;
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+            moveDir.z -= 1;
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            moveDir.x += 1;
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            moveDir.x -= 1;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveDir *= runSpeed;
+
+            cam.transform.localPosition = Vector3.Lerp(cameralook,transform.localPosition, new Vector3(0,2,0),crouchTransitionSpeed * Time.deltaTime);
+            cc.height = Mathf.Lerp(cc.height, 2, crouchTransitionSpeed * Time.deltaTime);
+            cc.center = Vector3.Lerp(cc.center,new Vector3(0,1,0),crouchTransitionSpeed * Time.deltaTime);
+
+        }
+        else if (Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            moveDir *= crouchSpeed;
+
+            cam.transform.localPosition = Vector3.Lerp(cameralook, transform.localPosition, new Vector3(0, 1, 0), crouchTransitionSpeed * Time.deltaTime);
+            cc.height = Mathf.Lerp(cc.height, 1.2f, crouchTransitionSpeed * Time.deltaTime);
+            cc.center = Vector3.Lerp(cc.center, new Vector3(0, 0.59f, 0), crouchTransitionSpeed * Time.deltaTime);
+        }
+        else
+        {
+            moveDir *= walkSpeed;
+
+            cam.transform.localPosition = Vector3.Lerp(cameralook, transform.localPosition, new Vector3(0, 2, 0), crouchTransitionSpeed * Time.deltaTime);
+            cc.height = Mathf.Lerp(cc.height, 2, crouchTransitionSpeed * Time.deltaTime);
+            cc.center = Vector3.Lerp(cc.center, new Vector3(0, 1, 0), crouchTransitionSpeed * Time.deltaTime);
+        }
+
+        if (cc.isGrounded)
+        {
+
+            yVelocity = 0;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                yVelocity = jumpForce;
+            }
+        }
+        else
+            yVelocity += gravityAcceleration;
+
+        moveDir.y = yVelocity;
+
+        moveDir = transform.TransformDirection(moveDir);
+        moveDir *= Time.deltaTime;
+
+        cc.Move(moveDir);
+    }
+}
