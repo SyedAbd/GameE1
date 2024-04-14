6c9f4c313dca6662bbf70c756843ab9a8d7db9e6
Yue Ding <ae8273@student.jamk.fi>
2024-04-14 19:59:21 +0300

    Inventory

---
+++ Assets/Scripts/Inventory/ItemSO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Item", menuName = "Survival Game/Inventory/New Item")]
+public class ItemSO : ScriptableObject
+{
+    public enum ItemType { Generic,Consumable, Weapon, MeleeWeapon}
+
+    [Header("Genenral")]
+
+    public ItemType itemType;
+    public Sprite icon;
+    public string itemName = "New Item";
+    public string description = "New Item Description";
+
+    public bool isStackable;
+    public int maxStack = 1;
+
+
+}
