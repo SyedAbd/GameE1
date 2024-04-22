b4ca7db6c5938c93f7be40b72152e23edbd71a3a
Xinyue Wang0206 <108703804+AmberWang92@users.noreply.github.com>
2024-04-22 23:32:58 +0300

    change light settings

--- Assets/Scripts/DayNightSystem.cs
+++ Assets/Scripts/DayNightSystem.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class DayNightSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//    public Light directionalLight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//    public float dayDurationInseconds = 24.0f; //Adjust the duration of a full day in seconds
+//    public int currentHour;
+//    float currentTimeOfDay = 0.0f;
+
+//    public List<SkyboxTimeMapping> timeMapping;
+
+
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        currentTimeOfDay += Time.deltaTime / dayDurationInseconds;
+//        currentTimeOfDay %= 1;
+
+//        currentHour = Mathf.FloorToInt(currentTimeOfDay * 24);
+
+//        directionalLight.transform.rotation = Quaternion.Euler(new Vector3((currentTimeOfDay * 360) - 90, 170, 0));
+
+//        UpdateSkybox();
+//    }
+//}
+
+
+//public class SkyboxTimeMapping
+//{
+//    public string phaseName;
+//    public int hour; // the hour of the day
+//    public Material skyboxMaterial;
+
+
 }
