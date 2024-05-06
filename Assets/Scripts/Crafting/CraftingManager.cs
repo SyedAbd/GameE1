c8326c73c6b1994373f82ec6e946a65816d9b572
Yue Ding <ae8273@student.jamk.fi>
2024-05-07 00:10:26 +0300

    menu

--- Assets/Scripts/Crafting/CraftingManager.cs
+++ Assets/Scripts/Crafting/CraftingManager.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CraftingManager : MonoBehaviour
 {
+    private InventoryManager inventory;
     public RecipeTemplate recipeTemplate;
     public CraftingRecipeSO[] recipes;
     public Transform contentHolder;
 
+
+
     private void Start()
     {
+        inventory = GetComponentInParent<InventoryManager>();
         GenerateRecipes();
     }
     public void GenerateRecipes()
@@ -17,8 +21,10 @@ public class CraftingManager : MonoBehaviour
         for (int i = 0; i < recipes.Length; i++)
         {
             RecipeTemplate recipe = Instantiate(recipeTemplate.gameObject,contentHolder).GetComponent<RecipeTemplate>();
+            recipe.recipe = recipes[i];
 
             recipe.nameText.text = recipes[i].recipeName;
+            recipe.timerText.text = "";
 
             for (int b = 0; b < recipes[i].requirements.Length; b++)
             {
@@ -32,12 +38,41 @@ public class CraftingManager : MonoBehaviour
 
     public void Try_Craft(RecipeTemplate template)
     {
-        
+        if (!HasResource() || isCrafting)
+            return;
     }
 
     public void Cancel(RecipeTemplate template)
     {
         
+    }
+
+    public bool HasRecourses(CraftingRecipeSO recipe)
+        
+    {
+        int[] stacksNeeded = null;
+        int[] stacksAvailable = null;
+        List<int>stacksNeededList = new List<int>();
+
+        for (int i = 0;i < recipe.requirements.Length;i++)
+        {
+            stacksNeededList.Add(recipe.requirements[i].amountNeeded);
+        }
+        stacksNeeded = stacksNeededList.ToArray();
+        stacksAvailable = new int[stacksNeeded.Length];
+
+        for (int i = 0;i< inventory.inventorySlots.Length;i++)
+        {
+            if (inventory.inventorySlots[i].data == recipe.requirements[i].data)
+            {
+                stacksAvailable[i] += inventory.inventorySlots[i].stackSize;
+            }
+        }
+
+
+
+
+
     }
 
 }
